6bfb3bb2a68189008751a0820504591a1c4ec9b5
genzaraky <amadou140589@gmail.com>
2020-06-19 11:43:19 +0100

    PortFolio project added

--- src/views/Projects.js
+++ src/views/Projects.js
@@ -5,6 +5,17 @@ import Fade from 'react-reveal/Fade';
 
 const projectList = [
   {
+    name: 'PortFolio',
+    description: 'This is my PortFolio Web app Project. The Portfolio is built with ReactJs, and Bulma Css Framework.',
+    images: [
+      'react.png', 'shop-2.png',
+    ],
+    technologies: ['HTML', 'CSS', 'JAVASCRIPT','ReactJs','Bulma'],
+    class: 'is-light',
+    demo_link: 'https://rawcdn.githack.com/genzaraki/shoptemplate/c3a12d5c081d2b240f7ff6834a62bf97efebc885/index.html',
+    github_link: 'https://github.com/tigamadou/shoptemplate',
+  },
+  {
     name: 'Shop Template',
     description: 'This Project is my Html/Css project for Capstone projects by Microverse. The pages built are for an online electronics shop and the users will be able to browse the products by category and also see the product details page.',
     images: [
@@ -17,11 +28,11 @@ const projectList = [
   },
   {
     name: 'Ashu Social NetWork',
-    description: 'Ashu is a Twitter-like social network where users can create, comment and like posts, but also follow other users. This app is build with Ruby On Rails.',
+    description: 'Ashu is a Twitter-like social network where users can create, comment and like posts, but also follow other users. This app is built with Ruby On Rails.',
     images: [
       '2.png', 'shop-2.png',
     ],
-    technologies: ['Ruby', 'Ruby on Rails', 'Html/Erb', 'Css', 'Javascript'],
+    technologies: ['Ruby', 'Ruby on Rails', 'Html/Erb', 'Css', 'Javascript','Bulma'],
     class: 'is-danger is-variant',
     demo_link: 'https://blooming-escarpment-87825.herokuapp.com/users/sign_in',
     github_link: 'https://github.com/tigamadou/ashu-social-network',
@@ -29,7 +40,7 @@ const projectList = [
   },
   {
     name: 'Human Right Talks',
-    description: 'Hrt is a Twitter like social Network. The user can create post, comment, like and follow other users.',
+    description: 'Hrt is a Twitter-like social network. The user can create posts, comment, like, and follow other users.',
     images: [
       'hrt.png', 'shop-2.png',
     ],
